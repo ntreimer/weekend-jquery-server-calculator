656cd12d56f555fa4d3b4ea15e3a4f3d60eca15d
Nate Treimer <natetreimer@gmail.com>
2021-03-13 22:14:45 -0600

    wrote GET and POST functions

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,20 @@ let calculation = {
     operation: ''
 }
 
+function calculationToServer(){
+    console.log('in displayHistory');
+    $.ajax({
+        type: 'POST',
+        url: '/calculate',
+        data: calculation
+    }).then(function(response){
+        console.log('back from POST:', response);
+        displayHistory;
+    }).catch(function(err){
+        alert('error:', err)
+    })//end AJAX
+}// end calculationToServer
+
 function displayHistory(){
     console.log('in displayHistory');
     $.ajax({
@@ -28,9 +42,26 @@ function onReady(){
     $('#subtractButton').on('click', updateOperation);
     $('#multiplyButton').on('click', updateOperation);
     $('#divideButton').on('click', updateOperation);
+    $('#equalButton').on('click', submitCalculation);
+    $('#clearButton').on('click', );
+    // dynamically created click handlers
 }// end onReady
 
 function updateOperation(){
     // update operation in calculation object
     calculation.operation = $(this).text();
-}
+}// end updateOperation
+
+function submitCalculation(){
+    calculation.input1 = $('#input1').val();
+    calculation.input2 = $('#input2').val();
+    if (calculation.input1 === '' || calculation.input2 === '' || calculation.operation === ''){
+        alert('Please complete both input fields and select an operation.')
+    }// end if
+    else {
+        alert('passed');
+        calculationToServer;
+        $('#input1').val('');
+        $('#input2').val('');
+    }
+}// end submitCalculation
