7449fab304a02ecf3d05318d6aba50922bfcdb4b
Nate Treimer <natetreimer@gmail.com>
2021-03-13 21:46:22 -0600

    added click handlers for operations

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,19 +1,36 @@
-console.log('JS');
 $(document).ready(onReady);
+
+let calculation = {
+    input1: '',
+    input2: '',
+    operation: ''
+}
 
 function displayHistory(){
     console.log('in displayHistory');
     $.ajax({
-        type: 'GET'
+        type: 'GET',
         url: '/calculate'
     }).then(function(response){
         console.log('back from GET:', response);
+        // clear previous history from DOM
+        // append response
     }).catch(function(err){
         alert('error:', err)
     })//end AJAX
 }// end displayHistory
 
 function onReady(){
-    console.log('jQuery');
+    // on page load
     displayHistory();
+    // click handlers
+    $('#addButton').on('click', updateOperation);
+    $('#subtractButton').on('click', updateOperation);
+    $('#multiplyButton').on('click', updateOperation);
+    $('#divideButton').on('click', updateOperation);
 }// end onReady
+
+function updateOperation(){
+    // update operation in calculation object
+    calculation.operation = $(this).text();
+}
